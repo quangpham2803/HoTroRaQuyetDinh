a75cd6e8e7086b21beee8469efd9e1db958b6ad1
Tung <125894748+TungNguyenThanh1921@users.noreply.github.com>
2023-04-27 08:49:49 +0700

    Update Script

---
+++ Assets/Scripts/Main/ListMenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject Item;
+    [SerializeField] Transform Content;
+
+
+} 
